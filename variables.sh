cfbd8fea672a7475544415a498197fe22bb8aede
rahim <rm.basha66@gmail.com>
2026-02-02 00:01:13 +0530

    added few lines

---
+++ variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=ram
+PERSON2=rahim
+
+echo "$PERSON1:: hello $PERSON2, How are you?"
+echo "$PERSON2:: hi $PERSON1, i am fine. How are you doing?"
+echo "$PERSON1:: Doing great. what are you learning $PERSON2?"
+echo "$PERSON2:: i am learning shell. what about you?"
