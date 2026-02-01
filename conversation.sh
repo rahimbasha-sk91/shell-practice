7cfa609cf51808200e4ca7a17ce4c9c15f675826
rahim <rm.basha66@gmail.com>
2026-02-01 23:52:04 +0530

    added few lines

--- conversation.sh
+++ conversation.sh
@@ -2,5 +2,5 @@
 
 echo "ram:: hello rahim, How are you?"
 echo "rahim:: hi ram, i am fine. How are you doing?"
-echo "ram:: Dong great. what are you learning rahim?"
+echo "ram:: Doing great. what are you learning rahim?"
 echo "rahim:: i am learning shell. what about you?"
