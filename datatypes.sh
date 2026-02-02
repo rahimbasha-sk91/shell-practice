a7d34a65c9848dbfd0ee95edda0bc2506f99f09f
rahim <rm.basha66@gmail.com>
2026-02-02 23:36:06 +0530

    added few lines

--- datatypes.sh
+++ datatypes.sh
@@ -12,3 +12,7 @@ echo "Sum is :$SUM"
 FRUITS=("Apple" "Banana" "orange")
 
 echo "Fruits are: ${FRUITS[@]}"
+
+echo "first fruit is :${FRUITS[0]}"
+echo "second fruit is :${FRUITS[1]}"
+echo "Third fruit is :${FRUITS[2]}"
